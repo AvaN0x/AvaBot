5d457042bdb3c7c11a8e5b2d17743739c84f7945
AvaN0x <clement.ricatte57@gmail.com>
2020-06-27 13:17:41 +0200

    fix warnings and add comments

--- Modules/SettingsCommands.cs
+++ Modules/SettingsCommands.cs
@@ -9,16 +9,16 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 
-#pragma warning disable CS1998
+//#pragma warning disable CS1998
 namespace AvaBot.Modules
 {
     // for commands to be available, and have the Context passed to them, we must inherit ModuleBase
     [Group("settings")]
     [Alias("set", "s")]
+    [RequireOwner]
     public class SettingsCommands : ModuleBase
     {
         [Command]
-        [RequireOwner]
         public async Task NoSettingFoundCommand()
         {
             EmbedBuilder embedMessage = new EmbedBuilder()
@@ -27,95 +27,118 @@ namespace AvaBot.Modules
             await ReplyAsync("", false, embedMessage.Build());
         }
 
-        // TODO need optimisation, reflection?! :)
-        [Command("textscan")]
+        [Group("textscan")]
         [Alias("scan")]
-        [RequireOwner]
-        public async Task SetTextScanCommand(string value = null)
+        public class SettingsCommands_Scan : ModuleBase
         {
-            bool flag;
-            if (Boolean.TryParse(value, out flag))
+            // TODO need optimisation, reflection?! :)
+            // //s scan --> display values
+            // //s scan bool --> set all to bool value
+            [Command]
+            public async Task SetTextScanCommand(string value = null)
             {
-                Program.settings.Get(Context.Guild.Id).modpackScan = flag;
-                Program.settings.Get(Context.Guild.Id).chehScan = flag;
-                Program.settings.Get(Context.Guild.Id).gf1Scan = flag;
-                Program.settings.Get(Context.Guild.Id).ineScan = flag;
-                Program.settings.SaveSettings();
-                EmbedBuilder embedMessage = new EmbedBuilder()
-                    .WithDescription("Value of **modpack** set to *" + flag + "*" +
-                        "\nValue of **cheh** set to *" + flag + "*" +
-                        "\nValue of **gf1** set to *" + flag + "*" +
-                        "\nValue of **ine** set to *" + flag + "*")
-                    .WithColor(255, 241, 185);
-                await ReplyAsync("", false, embedMessage.Build());
+                bool flag;
+                if (Boolean.TryParse(value, out flag))
+                {
+                    Program.settings.Get(Context.Guild.Id).modpackScan = flag;
+                    Program.settings.Get(Context.Guild.Id).chehScan = flag;
+                    Program.settings.Get(Context.Guild.Id).gf1Scan = flag;
+                    Program.settings.Get(Context.Guild.Id).ineScan = flag;
+                    Program.settings.SaveSettings();
+                    EmbedBuilder embedMessage = new EmbedBuilder()
+                        .WithDescription("Value of **modpack** set to *" + flag + "*" +
+                            "\nValue of **cheh** set to *" + flag + "*" +
+                            "\nValue of **gf1** set to *" + flag + "*" +
+                            "\nValue of **ine** set to *" + flag + "*")
+                        .WithColor(255, 241, 185);
+                    await ReplyAsync("", false, embedMessage.Build());
+                }
+                else
+                {
+                    EmbedBuilder embedMessage = new EmbedBuilder()
+                        .WithDescription("Value of **modpack** is *" + Program.settings.Get(Context.Guild.Id).modpackScan + "*" +
+                            "\nValue of **cheh** is *" + Program.settings.Get(Context.Guild.Id).chehScan + "*" +
+                            "\nValue of **gf1** is *" + Program.settings.Get(Context.Guild.Id).gf1Scan + "*" +
+                            "\nValue of **ine** is *" + Program.settings.Get(Context.Guild.Id).ineScan + "*")
+                        .WithColor(255, 241, 185);
+                    await ReplyAsync("", false, embedMessage.Build());
+                }
             }
-            else
-            {
-                EmbedBuilder embedMessage = new EmbedBuilder()
-                    .WithDescription("Value of **modpack** is *" + Program.settings.Get(Context.Guild.Id).modpackScan + "*" +
-                        "\nValue of **cheh** is *" + Program.settings.Get(Context.Guild.Id).chehScan + "*" +
-                        "\nValue of **gf1** is *" + Program.settings.Get(Context.Guild.Id).gf1Scan + "*" +
-                        "\nValue of **ine** is *" + Program.settings.Get(Context.Guild.Id).ineScan + "*")
-                    .WithColor(255, 241, 185);
-                await ReplyAsync("", false, embedMessage.Build());
-            }
-        }
 
+            // //s scan modpack --> display value
+            // //s scan modpack bool --> set modpackScan to bool value
             [Command("modpack")]
             [RequireOwner]
             public async Task SetModpackScanCommand(string value = null)
             {
                 var setting = Program.settings.Get(Context.Guild.Id).modpackScan;
-                SetBoolean("modpack", ref setting, value);
+                var embedMessage = SetBoolean("modpack", ref setting, value);
                 Program.settings.Get(Context.Guild.Id).modpackScan = setting;
+                Program.settings.SaveSettings();
+                await ReplyAsync("", false, embedMessage.Build());
             }
 
+            // //s scan cheh --> display value
+            // //s scan cheh bool --> set chehScan to bool value
             [Command("cheh")]
             [RequireOwner]
             public async Task SetChehScanCommand(string value = null)
             {
                 var setting = Program.settings.Get(Context.Guild.Id).chehScan;
-                SetBoolean("cheh", ref setting, value);
+                var embedMessage = SetBoolean("cheh", ref setting, value);
                 Program.settings.Get(Context.Guild.Id).chehScan = setting;
+                Program.settings.SaveSettings();
+                await ReplyAsync("", false, embedMessage.Build());
             }
 
+            // //s scan gf1 --> display value
+            // //s scan gf1 bool --> set gf1Scan to bool value
             [Command("gf1")]
             [RequireOwner]
             public async Task SetGf1ScanCommand(string value = null)
             {
                 var setting = Program.settings.Get(Context.Guild.Id).gf1Scan;
-                SetBoolean("gf1", ref setting, value);
+                var embedMessage = SetBoolean("gf1", ref setting, value);
                 Program.settings.Get(Context.Guild.Id).gf1Scan = setting;
+                Program.settings.SaveSettings();
+                await ReplyAsync("", false, embedMessage.Build());
             }
 
+            // //s scan ine --> display value
+            // //s scan ine bool --> set ineScan to bool value
             [Command("ine")]
             [RequireOwner]
             public async Task SetIneScanCommand(string value = null)
             {
                 var setting = Program.settings.Get(Context.Guild.Id).ineScan;
-                SetBoolean("ine", ref setting, value);
+                var embedMessage = SetBoolean("ine", ref setting, value);
                 Program.settings.Get(Context.Guild.Id).ineScan = setting;
+                Program.settings.SaveSettings();
+                await ReplyAsync("", false, embedMessage.Build());
             }
 
-        public void SetBoolean(string settingName, ref bool setting, string value)
+        }
+
+
+
+        public static EmbedBuilder SetBoolean(string settingName, ref bool setting, string value)
         {
+            EmbedBuilder embedMessage;
             bool flag;
             if (Boolean.TryParse(value, out flag))
             {
                 setting = flag;
-                Program.settings.SaveSettings();
-                EmbedBuilder embedMessage = new EmbedBuilder()
+                embedMessage = new EmbedBuilder()
                     .WithDescription("Value of **" + settingName + "** set to *" + flag + "*")
                     .WithColor(255, 241, 185);
-                ReplyAsync("", false, embedMessage.Build());
             }
             else
             {
-                EmbedBuilder embedMessage = new EmbedBuilder()
+                embedMessage = new EmbedBuilder()
                     .WithDescription("Value of **" + settingName + "** is *" + setting + "*")
                     .WithColor(255, 241, 185);
-                ReplyAsync("", false, embedMessage.Build());
             }
+            return embedMessage;
         }
     }
 }
