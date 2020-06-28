69ad599d5c00b19838bb7068b5d08d3a30589d72
AvaN0x <clement.ricatte57@gmail.com>
2020-06-28 23:24:15 +0200

    Add me command, and better display of date

--- Modules/RandomCommands.cs
+++ Modules/RandomCommands.cs
@@ -1,4 +1,4 @@
-using Discord;
+﻿using Discord;
 using Discord.Net;
 using Discord.WebSocket;
 using Discord.Commands;
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
+using System.Globalization;
 
 namespace AvaBot.Modules
 {
@@ -16,14 +17,43 @@ namespace AvaBot.Modules
     {
         [Command("createdat")]
         public async Task CreatedAtCommand()
-            => await ReplyAsync(Context.User + " where created on " + Context.User.CreatedAt);
+            => await ReplyAsync(Context.User + " where created on " + Context.User.CreatedAt.ToString("F", DateTimeFormatInfo.InvariantInfo));
+
+        [Command("me")]
+        [Alias("info")]
+        public async Task MeCommand(SocketGuildUser user = null)
+        {
+            if (user == null)
+                user = (SocketGuildUser)Context.User;
+
+            EmbedBuilder embedMessage = new EmbedBuilder()
+                .WithTitle(user.Username + " informations")
+                .AddField("Global", "" +
+                    "• ToString() : *" + user.ToString() + "*" + 
+                    "\n• CreatedAt : *" + user.CreatedAt.ToString("F", DateTimeFormatInfo.InvariantInfo) + "*" + 
+                    "\n• Activity : *" + user.Activity + "*" + 
+                    "\n• Status : *" + user.Status + "*" + 
+                    "\n• GetDefaultAvatarUrl() : *[link](" + user.GetDefaultAvatarUrl() + ")*" + 
+                    "\n• GetAvatarUrl() : *[link](" + user.GetAvatarUrl() + ")*" + 
+                    "", false)
+                .AddField("Guild", "" +
+                    "\n• Guild : *" + user.Guild + "*" + 
+                    "\n• Nickname : *" + user.Nickname + "*" + 
+                    "\n• JoinedAt : *" + ((DateTimeOffset) user.JoinedAt).ToString("F", DateTimeFormatInfo.InvariantInfo) + "*" + 
+                    "", false)
+                .WithFooter("github.com/AvaN0x", "https://avatars3.githubusercontent.com/u/27494805?s=460&v=4")
+                .WithColor(255, 241, 185);
+
+            await ReplyAsync("", false, embedMessage.Build());
+
+        }
 
         [Command("github")]
         [Alias("avan0x")]
         public async Task GitHubCommand()
         {
             EmbedBuilder embedMessage = new EmbedBuilder()
-                .WithTitle("AvaN0x - Cl�ment RICATTE")
+                .WithTitle("AvaN0x - Clément RICATTE")
                 .AddField("Github", "[github.com/AvaN0x](https://github.com/AvaN0x)", false)
                 .AddField("WebSite", "[avan0x.github.io](https://avan0x.github.io/)", false)    // true - for inline
                 .WithFooter("AvaN0x", "https://avatars3.githubusercontent.com/u/27494805?s=460&v=4")
@@ -33,6 +63,26 @@ namespace AvaBot.Modules
             await ReplyAsync("", false, embedMessage.Build());
             await Context.Message.DeleteAsync();
         }
+
+        [Command("timer", RunMode = RunMode.Async)]
+        public async Task TestCommand(int duree)
+        {
+            var date = DateTime.Now.AddSeconds(duree);
+            await ReplyAsync(DateTime.Now.ToString("T") + " : Date set to " + date.ToString("T"));
+            while (DateTime.Now <= date)
+                await Task.Delay(1);
+            await ReplyAsync(DateTime.Now.ToString("T") + " : " + Context.User.Mention + " •a devrait faire " + duree + " secondes");
+        }
+
+        //[Command("test")]
+        //public async Task TestCommand()
+        //{
+        //    //foreach (var m in ((SocketGuild)Context.Guild).Users)
+        //    //    await m.SendMessageAsync("Quoi de pire qu'un ping? Un mp.");
+
+        //    //await ((SocketGuild)Context.Guild).GetUser(261255005162438656).SendMessageAsync("GF1 sacam");
+        //    //await Context.User.SendMessageAsync("Quoi de pire qu'un ping? Un mp.");
+        //}
 
     }
 }
