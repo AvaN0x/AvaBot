8455ea1af3d0cf07d939e0c9589d4bd174519d05
AvaN0x <clement.ricatte57@gmail.com>
2020-07-09 15:22:00 +0200

    Change help commands, now display the shortest command prompt

--- Modules/HelpCommands.cs
+++ Modules/HelpCommands.cs
@@ -29,55 +29,34 @@ namespace AvaBot.Modules
         [Command]
         [Summary("Give informations about every commands")]
         public async Task HelpCommand()
-        {
-            List<CommandInfo> commands = _commands.Commands.ToList();
-            EmbedBuilder embedBuilder = new EmbedBuilder()
-                .WithTitle("Commands help")
-                .WithDescription("" +
-                    "The prefix is `" + _config["Prefix"] + "`.")
-                .WithFooter("github.com/AvaN0x", "https://avatars3.githubusercontent.com/u/27494805?s=460&v=4")
-                .WithColor(255, 241, 185);
-
-            foreach (var module in _commands.Modules)
-            {
-                var fieldContent = "";
-                foreach (var command in module.Commands)
-                {
-                    fieldContent += "• `" + _config["Prefix"] + command.Aliases.First() + string.Join("", command.Parameters.Select(p => " [" + p.Name + "]")) +
-                        "` : " + (command.Summary ?? "No description available") +
-                        string.Concat(command.Parameters.Select(p => "\n[" + p.Name + "] : *" + (p.Summary ?? "No description available") + "*")) + "\n";
-                }
-                embedBuilder.AddField(module.Summary ?? "No summary available", fieldContent);
-            }
-            await ReplyAsync("", false, embedBuilder.Build());
-        }
+            => await ShowModuleHelp("Commands help", _commands.Modules.Where(m => !m.Aliases.First().Contains("settings")));
 
 
         [Command("settings")]
         [Alias("set" , "s")]
         [Summary("Give informations about settings commands")]
         public async Task HelpSettingsCommand()
+            => await ShowModuleHelp("Commands help - Settings", _commands.Modules.Where(m => m.Aliases.First().Contains("settings")));
+
+        private async Task ShowModuleHelp(string title, IEnumerable<ModuleInfo> modules)
         {
-            EmbedBuilder embedMessage = new EmbedBuilder()
-                .WithTitle("Commands help - settings")
-                .WithDescription("Actual permission needed : Owner")
-                .AddField("See value", "`//s [category / command]`" +
-                    "\nExample : " +
-                    "\n`//s` -> Value of every command" +
-                    "\n`//s scan` -> Value of every command in scan category" +
-                    "\n`//s scan ine` -> Value of ine command" +
-                    "\n`//s mute` -> Value of mute //and unmute command" +
-                    "", false)
-                .AddField("Edit value", "`//s [category / command] value`" +
-                    "\nExample : " +
-                    "\n`//s scan false` -> Value of every command in scan category set to false" +
-                    "\n`//s scan ine false` -> Value of ine command set to true" +
-                    "\n`//s mute false` -> Value of mute and unmute command set to false" +
-                    "", false)
+            EmbedBuilder embedBuilder = new EmbedBuilder()
+                .WithTitle(title)
                 .WithFooter("github.com/AvaN0x", "https://avatars3.githubusercontent.com/u/27494805?s=460&v=4")
                 .WithColor(255, 241, 185);
 
-            await ReplyAsync("", false, embedMessage.Build());
+            foreach (var module in modules)
+            {
+                var fieldContent = "";
+                foreach (var command in module.Commands)
+                {
+                    fieldContent += "• `" + _config["Prefix"] + command.Aliases.OrderBy(a => a.Length).First() + string.Join("", command.Parameters.Select(p => " [" + p.Name + "]")) +
+                        "` : " + (command.Summary ?? "No description available") +
+                        string.Concat(command.Parameters.Select(p => "\n[" + p.Name + "] : *" + (p.Summary ?? "No description available") + "*")) + "\n";
+                }
+                embedBuilder.AddField(module.Summary ?? "No summary available", fieldContent);
+            }
+            await ReplyAsync("", false, embedBuilder.Build());
         }
     }
 }
